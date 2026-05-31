ba712f471816e8306a0dc67b6814245c12d0b762
abhisheky8002-source <abhisheky8002@gmail.com>
2026-05-31 18:09:24 +0530

    updated images with cloudinary links

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -52,7 +52,7 @@ export default function About() {
 
               <div className="w-full h-full rounded-[2rem] overflow-hidden border border-slate-100 shadow-xl bg-slate-50">
                 <img
-                  src="/src/assets/images/dr_debuka_portrait_1780224205205.png"
+                  src="https://res.cloudinary.com/dr5obadvt/image/upload/v1780229254/587802656_17842679394634714_3646617993978590076_n_udyfjs.jpg"
                   alt="Dr. Ekansh Debuka consulting"
                   className="w-full h-full object-cover object-top"
                   referrerPolicy="no-referrer"
